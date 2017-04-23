c65dcfcc40cc0a72b7ea315f0c622a7d67cb7494
Toan Nguyen <toannv0616@gmail.com>
2017-04-23 21:57:38 +0700

    Copy Login page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var logger = require('morgan');
-
+var path = require('path');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -10,8 +10,14 @@ app.use(logger('dev'));
 app.set('views', './app/views');
 app.set('view engine', 'hbs');
 
+app.use(express.static(path.join(__dirname, 'app', 'public')));
+
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
 });
 
 app.listen(port, function () {
